31673cc7b1fb5442ec14f1f300e5338db8831dc3
Patrick Langley <patrickdlangley@gmail.com>
2020-12-21 15:57:26 -0500

    Add project files.

---
+++ P4000Source.cpp
@@ -0,0 +1,19 @@
+/*
+Patrick Langley
+Project 4000
+12/2/2020
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "\nThis is a new program";
+	
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
